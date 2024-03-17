50a36863a426d07409226b1d180e89a38744194c
Romain Durade <durade.romain@gmail.com>
2024-03-17 13:10:02 +0100

    add dirtdetect sensor

--- src/ArduRoomba.cpp
+++ src/ArduRoomba.cpp
@@ -28,6 +28,9 @@ bool ArduRoomba::_parseStreamBuffer(uint8_t *packets, int len,
     case ARDUROOMBA_SENSOR_MODE:
       infos->mode = (int)_parseOneByteStreamBuffer(packets, i);
       break;
+    case ARDUROOMBA_SENSOR_DIRTDETECT:
+      infos->dirtdetect = (int)_parseOneByteStreamBuffer(packets, i);
+      break;
     case ARDUROOMBA_SENSOR_CHARGINGSTATE:
       infos->chargingState = (int)_parseOneByteStreamBuffer(packets, i);
       break;
@@ -192,6 +195,8 @@ bool ArduRoomba::isDropWheelRight() { return _stateInfos.wheelDropRight; }
 bool ArduRoomba::isDropWheelLeft() { return _stateInfos.wheelDropLeft; }
 
 int ArduRoomba::getMode() { return _stateInfos.mode; }
+
+int ArduRoomba::getDirtDetect() { return _stateInfos.dirtdetect; }
 
 int ArduRoomba::getChargingState() { return _stateInfos.chargingState; }
 
@@ -395,6 +400,10 @@ bool ArduRoomba::getSerialData(char packetID, uint8_t *destbuffer, int len) {
 
 bool ArduRoomba::reqMode(RoombaInfos *infos) {
   return _reqNByteSensorData(ARDUROOMBA_SENSOR_MODE, 1, infos);
+}
+
+bool ArduRoomba::reqDirtDetect(RoombaInfos *infos) {
+  return _reqNByteSensorData(ARDUROOMBA_SENSOR_DIRTDETECT, 1, infos);
 }
 
 bool ArduRoomba::reqChargingState(RoombaInfos *infos) {
--- src/ArduRoomba.h
+++ src/ArduRoomba.h
@@ -25,7 +25,6 @@
 #define ARDUROOMBA_ERROR(msg, ...)
 #endif
 
-
 #define ARDUROOMBA_SERIAL_READ_TIMEOUT 100
 
 #define ARDUROOMBA_REFRESH_DELAY 90
@@ -50,36 +49,34 @@
 #define ARDUROOMBA_SENSOR_CLIFFRIGHT 12
 #define ARDUROOMBA_SENSOR_CLIFFFRONTRIGHT 11
 #define ARDUROOMBA_SENSOR_WHEELOVERCURRENTS 14
+#define ARDUROOMBA_SENSOR_DIRTDETECT 15
 
-
-class ArduRoomba
-{
+class ArduRoomba {
 public:
   // Constructor
   ArduRoomba(int rxPin, int txPin, int brcPin); // Constructor
 
   // Custom structs to use in main code.
-  struct Note
-  {
+  struct Note {
     byte noteNumber;   // MIDI note number (31 - 127)
     byte noteDuration; // Duration of the note in 1/64th of a second
   };
 
-  struct Song
-  {
+  struct Song {
     byte songNumber; // Song number (0 - 4)
     byte songLength; // Number of notes in the song (1 - 16)
     Note notes[16];  // Array of notes, up to 16
   };
 
-  struct RoombaInfos
-  {
+  struct RoombaInfos {
     int mode;
     int chargingState;
     int voltage;
     unsigned int temperature;
     int batteryCapacity;
     int batteryCharge;
+    int dirtdetect;
+
     bool wall;
     bool cliffLeft;
     bool cliffFrontLeft;
@@ -88,18 +85,17 @@ public:
 
     bool wheelRightOvercurrent;
     bool wheelLeftOvercurrent;
-    bool mainBrushOvercurrent;     
-    bool sideBrushOvercurrent; 
+    bool mainBrushOvercurrent;
+    bool sideBrushOvercurrent;
     bool vacuumOvercurrent; // no wacuum for serie 600
 
-    bool bumpRight;              // Bump Right ?
-    bool bumpLeft;               // Bump Left?
-    bool wheelDropRight;         // Wheel Drop Right ?
-    bool wheelDropLeft;          // Wheel Drop Left ?
+    bool bumpRight;      // Bump Right ?
+    bool bumpLeft;       // Bump Left?
+    bool wheelDropRight; // Wheel Drop Right ?
+    bool wheelDropLeft;  // Wheel Drop Left ?
   };
 
-  struct ScheduleStore
-  {
+  struct ScheduleStore {
     byte days;
     byte sunHour;
     byte sunMinute;
@@ -118,36 +114,44 @@ public:
   };
 
   // OI commands
-  void start();                                      // Start the OI
-  void baud(char baudCode);                          // Set the baud rate
-  void safe();                                       // Put the OI into Safe mode
-  void reset();                                      // Send "reset" command (equivalent to battery replacment)
-  void full();                                       // Put the OI into Full mode
-  void clean();                                      // Start the cleaning mode
-  void maxClean();                                        // Start the maximum time cleaning mode
-  void spot();                                       // Start the spot cleaning mode
-  void seekDock();                                   // Send the robot to the dock
+  void start();             // Start the OI
+  void baud(char baudCode); // Set the baud rate
+  void safe();              // Put the OI into Safe mode
+  void reset();    // Send "reset" command (equivalent to battery replacment)
+  void full();     // Put the OI into Full mode
+  void clean();    // Start the cleaning mode
+  void maxClean(); // Start the maximum time cleaning mode
+  void spot();     // Start the spot cleaning mode
+  void seekDock(); // Send the robot to the dock
   void schedule(ScheduleStore scheduleData);         // Set the schedule
   void setDayTime(char day, char hour, char minute); // Set the day and time
   void power();                                      // Power down the OI
 
   // Actuator commands
-  void drive(int velocity, int radius);                                         // Drive the robot
-  void driveDirect(int rightVelocity, int leftVelocity);                        // Drive the robot directly
-  void drivePWM(int rightPWM, int leftPWM);                                     // Drive the robot with PWM
-  void motors(byte data);                                                       // Control the motors
-  void pwmMotors(char mainBrushPWM, char sideBrushPWM, char vacuumPWM);         // Control the PWM of the motors
-  void leds(int ledBits, int powerColor, int powerIntensity);                   // Control the LEDs
-  void schedulingLeds(int weekDayLedBits, int scheduleLedBits);                 // Control the scheduling LEDs
-  void digitLedsRaw(int digitThree, int digitTwo, int digitOne, int digitZero); // Control the digit LEDs
-  void song(Song songData);                                                     // Load a song
-  void play(int songNumber);                                                    // Play a song
+  void drive(int velocity, int radius); // Drive the robot
+  void driveDirect(int rightVelocity,
+                   int leftVelocity);       // Drive the robot directly
+  void drivePWM(int rightPWM, int leftPWM); // Drive the robot with PWM
+  void motors(byte data);                   // Control the motors
+  void pwmMotors(char mainBrushPWM, char sideBrushPWM,
+                 char vacuumPWM); // Control the PWM of the motors
+  void leds(int ledBits, int powerColor,
+            int powerIntensity); // Control the LEDs
+  void schedulingLeds(int weekDayLedBits,
+                      int scheduleLedBits); // Control the scheduling LEDs
+  void digitLedsRaw(int digitThree, int digitTwo, int digitOne,
+                    int digitZero); // Control the digit LEDs
+  void song(Song songData);         // Load a song
+  void play(int songNumber);        // Play a song
 
   // Input commands
-  void sensors(char packetID);                      // Request a sensor packet
-  void queryList(byte numPackets, byte *packetIDs); // Request a list of sensor packets
-  bool getSerialData(char packetID, uint8_t* destbuffer, int len); // Request a sensor packet and fill buffer with response
-  
+  void sensors(char packetID); // Request a sensor packet
+  void queryList(byte numPackets,
+                 byte *packetIDs); // Request a list of sensor packets
+  bool getSerialData(
+      char packetID, uint8_t *destbuffer,
+      int len); // Request a sensor packet and fill buffer with response
+
   void queryStream();
   void resetStream();
   bool refreshData();
@@ -163,9 +167,10 @@ public:
   bool isBumpLeft();
   bool isDropWheelRight();
   bool isDropWheelLeft();
-  int getMode();                                        // Request sensor packet "mode"
-  int getChargingState();                               // Request sensor packet "charging state"                  // Request sensor packet "voltage" (voltage of Roomba's battery in milivolts)
-  unsigned int getTemperature();                       // Request sensor packet "temperature" (temperature of Roomba's battery in degrees Celsius)
+  int getMode();
+  int getDirtDetect();
+  int getChargingState();
+  unsigned int getTemperature();
   int getBatteryCapacity();
   int getBatteryCharge();
   int getVoltage();
@@ -176,20 +181,21 @@ public:
   bool isVacuumOvercurrent();
 
   // sensor request
-  bool reqMode(RoombaInfos *infos);                                        // Request sensor packet "mode"
-  bool reqChargingState(RoombaInfos *infos);                               // Request sensor packet "charging state"
-  bool reqVoltage(RoombaInfos *infos);                                     // Request sensor packet "voltage" (voltage of Roomba's battery in milivolts)
-  bool reqTemperature(RoombaInfos *infos);                          // Request sensor packet "temperature" (temperature of Roomba's battery in degrees Celsius)
-  bool reqBatteryCharge(RoombaInfos *infos);                                 // Request sensor packet "battery charge" (the current charge of Roomba's battery in miliamp-hours)
-  bool reqBatteryCapacity(RoombaInfos *infos);  
-  bool reqWall(RoombaInfos *infos);                               // Request sensor packet "battery capacity" (the estimated charge capacity of Roomba's battery in miliamp-hours)
+  bool reqMode(RoombaInfos *infos);
+  bool reqDirtDetect(RoombaInfos *infos);
+  bool reqChargingState(RoombaInfos *infos);
+  bool reqVoltage(RoombaInfos *infos);
+  bool reqTemperature(RoombaInfos *infos);
+  bool reqBatteryCharge(RoombaInfos *infos);
+  bool reqBatteryCapacity(RoombaInfos *infos);
+  bool reqWall(RoombaInfos *infos);
   bool reqCliffLeft(RoombaInfos *infos);
   bool reqCliffFrontLeft(RoombaInfos *infos);
   bool reqCliffRight(RoombaInfos *infos);
   bool reqCliffFrontRight(RoombaInfos *infos);
-  bool reqBumpAndWeelsDrops(RoombaInfos *infos);  // Request sensor packet "Bumps and Wheel Drops" (the state of the bumper and wheel drop sensor)
+  bool reqBumpAndWeelsDrops(RoombaInfos *infos);
   bool reqWeelsOvercurrents(RoombaInfos *infos);
-  
+
   // Custom commands
   void roombaSetup(); // Setup the Roomba
   void goForward();   // Move the Roomba forward
@@ -201,36 +207,36 @@ public:
 private:
   const byte _zero = 0x00;
   int _rxPin, _txPin, _brcPin;
-  SoftwareSerial _irobot; // SoftwareSerial instance for communication with the Roomba
-  
+  SoftwareSerial
+      _irobot; // SoftwareSerial instance for communication with the Roomba
+
   long _nextRefresh;
   long _lastSuccedRefresh;
   RoombaInfos _stateInfos;
 
   uint8_t _streamBuffer[100] = {};
-  int _nbSensorsStream = 13;
+  int _nbSensorsStream = 14;
   int _streamBufferSize = 0;
-  char _sensorsStream[20] = {
-    ARDUROOMBA_SENSOR_MODE,
-    ARDUROOMBA_SENSOR_CHARGINGSTATE,
-    ARDUROOMBA_SENSOR_TEMPERATURE,
-    ARDUROOMBA_SENSOR_VOLTAGE,
-    ARDUROOMBA_SENSOR_BATTERYCHARGE,
-    ARDUROOMBA_SENSOR_BATTERYCAPACITY,
-    ARDUROOMBA_SENSOR_BUMPANDWEELSDROPS,
-    ARDUROOMBA_SENSOR_WALL,
-    ARDUROOMBA_SENSOR_CLIFFLEFT,
-    ARDUROOMBA_SENSOR_CLIFFFRONTLEFT,
-    ARDUROOMBA_SENSOR_CLIFFRIGHT,
-    ARDUROOMBA_SENSOR_CLIFFFRONTRIGHT,
-    ARDUROOMBA_SENSOR_WHEELOVERCURRENTS
-  };
-  
+  char _sensorsStream[20] = {ARDUROOMBA_SENSOR_MODE,
+                             ARDUROOMBA_SENSOR_CHARGINGSTATE,
+                             ARDUROOMBA_SENSOR_TEMPERATURE,
+                             ARDUROOMBA_SENSOR_VOLTAGE,
+                             ARDUROOMBA_SENSOR_BATTERYCHARGE,
+                             ARDUROOMBA_SENSOR_BATTERYCAPACITY,
+                             ARDUROOMBA_SENSOR_BUMPANDWEELSDROPS,
+                             ARDUROOMBA_SENSOR_WALL,
+                             ARDUROOMBA_SENSOR_CLIFFLEFT,
+                             ARDUROOMBA_SENSOR_CLIFFFRONTLEFT,
+                             ARDUROOMBA_SENSOR_CLIFFRIGHT,
+                             ARDUROOMBA_SENSOR_CLIFFFRONTRIGHT,
+                             ARDUROOMBA_SENSOR_WHEELOVERCURRENTS,
+                             ARDUROOMBA_SENSOR_DIRTDETECT};
+
   bool _reqNByteSensorData(char packetID, int len, RoombaInfos *infos);
   bool _readStream();
-  bool _parseStreamBuffer(uint8_t* packets, int len, RoombaInfos *infos);
-  uint8_t _parseOneByteStreamBuffer(uint8_t* packets, int& start);
-  int _parseTwoByteStreamBuffer(uint8_t* packets, int& start);
+  bool _parseStreamBuffer(uint8_t *packets, int len, RoombaInfos *infos);
+  uint8_t _parseOneByteStreamBuffer(uint8_t *packets, int &start);
+  int _parseTwoByteStreamBuffer(uint8_t *packets, int &start);
 };
 
 #endif
